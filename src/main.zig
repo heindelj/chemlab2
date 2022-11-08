8933719da4e5fc8bfeabb3de78a56c962119572f
Joseph <heindelj@lbl.gov>
2022-11-07 23:54:37 -0800

    added glm

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,7 @@ const rlm = @import("raylib-math");
 const cl = @import("chemlab.zig");
 const std = @import("std");
 const print = std.debug.print;
+const glm = @import("glm.zig");
 
 pub fn main() anyerror!void {
     const screenWidth = 1440;
@@ -10,6 +11,12 @@ pub fn main() anyerror!void {
 
     rl.InitWindow(screenWidth, screenHeight, "chemlab");
     rl.SetTargetFPS(60);
+
+	var v: glm.Vec3 = glm.vec3(1.0, 2.0, 3.0);
+	var v2: rl.Vector3 = rl.Vector3{.x=1.0, .y=2.0, .z=3.0};
+
+	print("{} {} {}", .{v.vals[0], v.vals[1], v.vals[2]});
+	print("{} {} {}", .{v2.x, v2.y, v2.z});
 
     while (!rl.WindowShouldClose()) {
         rl.BeginDrawing();
